2e8442cc3c4de7fcd47bccc8e6ebb5d986406aa3
roshhhhhh20 <153496944+roshhhhhh20@users.noreply.github.com>
2026-05-13 17:30:26 +0530

    Create bda5_suvereducer.java

---
+++ bda5_suvereducer.java
@@ -0,0 +1,20 @@
+package titanic;
+import java.io.IOException;
+
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class SurvivorReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+    public void reduce(Text key, Iterable<IntWritable> values, Context context)
+            throws IOException, InterruptedException {
+
+        int sum = 0;
+
+        for (IntWritable val : values) {
+            sum += val.get();
+        }
+
+        context.write(key, new IntWritable(sum));
+    }
+}
