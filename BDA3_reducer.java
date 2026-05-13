58b954b51067b815670d9ab9e73b2309683fa29d
roshhhhhh20 <153496944+roshhhhhh20@users.noreply.github.com>
2026-05-13 16:50:19 +0530

    Create BDA3_reducer.java

---
+++ BDA3_reducer.java
@@ -0,0 +1,12 @@
+package grader;
+import java.io.IOException;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class GradeReducer extends Reducer<Text, Text, Text, Text> {
+    public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+        for (Text val : values) {
+            context.write(key, val);
+        }
+    }
+}
